c891ea7f791785ec58f605e92d5a1aac455c1b24
Ethan Henderson <ethan@zbee.me>
2014-09-24 07:52:51 -0600

    +FF 4chan thread styles
    The beginning of 4chan thread styles.

---
+++ data/4chan-thread-style.js
@@ -0,0 +1,118 @@
+let loc = window.location;
+if (/http(?:s)?\:\/\/boards\.4chan\.org\/([a-z]*)\/thread\/([0-9]*)(?:\#[0-9a-z]*)?/.test(loc)) {
+  //Store thread stats, placed here because it's container is removed next.
+  let THREAD_STATS = [];
+  THREAD_STATS[0] = $("span[data-tip*='Replies']").html();
+  THREAD_STATS[1] = $("span[data-tip*='Images']").html();
+  THREAD_STATS[2] = $("span[data-tip*='Page']").html();
+
+  //Store ReCAPTCHA image.
+  let RECAPTCHA = $("#recaptcha_image").html();
+
+  //Get rid of navigation.
+  $("#boardNavDesktop").remove();
+  $("#boardNavDesktopFoot").remove();
+  $("#boardNavMobile").remove();
+  $(".navLinks").remove();
+  $("#togglePostFormLink").remove();
+  $(".bottomCtrl").remove();
+  $("#absbot").remove();
+  $("#postForm").remove();
+  $(".button").remove();
+  $(".mobile").remove();
+
+  //Store what the board stuff is (board image, title, subtitle), then remove it.
+  let BOARD = [];
+  BOARD["BANNER"] = "//s.4cdn.org/image/title/" + $("#bannerCnt").attr("data-src");
+  BOARD["TITLE"] = $(".boardTitle").html();
+  BOARD["SUBTITLE"] = $(".boardSubtitle").html();
+  $(".boardBanner").remove();
+
+  //Get rid of <hr> tags on page.
+  $("hr").remove();
+
+  //Get rid of all of the ads and the ad pleas ([Advertise on 4chan]).
+  $(".ad-cnt").remove();
+  $(".ad-plea").remove();
+
+  //Store the blotter information (recent blog posts), then remove it
+  let BLOTTER = [];
+  BLOTTER[0] = $("#blotter-msgs tr:nth-child(2) td:nth-child(1)").html() + "~$" + $("#blotter-msgs tr:nth-child(1) td:nth-child(2)").html();
+  BLOTTER[1] = $("#blotter-msgs tr:nth-child(2) td:nth-child(2)").html() + "~$" + $("#blotter-msgs tr:nth-child(2) td:nth-child(2)").html();
+  BLOTTER[2] = $("#blotter-msgs tr:nth-child(2) td:nth-child(3)").html() + "~$" + $("#blotter-msgs tr:nth-child(3) td:nth-child(2)").html();
+  $("#blotter").remove();
+
+  //At this point, we only have the posts, now for the reformatting.
+
+  //Remove current stylesheets.
+  $('link[rel*="stylesheet"]').remove();
+
+  //Remove all 4chan Javascripts.
+  $("script[src*='4cdn']").remove();
+  $("script[src*='4chan']").remove();
+
+  //Include Bootstrap CSS and FontAwesome CSS
+  $('head').append('<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet"/>');
+  $('head').append('<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"/>');
+
+  //Include JQuery and Bootstrap JS (?)
+  ////$("body").append("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>");
+  ////$("body").append("<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>");
+
+  //Convert the thread container to the bootstrap equivalent.
+  $(".thread").addClass("col-md-10");
+
+  //Add sidebar, before the thread container.
+  $("body").prepend("<div class='col-md-2 text-center' id='sb'></div>");
+  $("#sb").append("<h1>" + BOARD["TITLE"] + "</h1>");
+  $("#sb").append(BOARD["SUBTITLE"]);
+  $("#sb").append("<hr>");
+  $("#sb").append("<img src='" + BOARD["BANNER"] + "' width='100%' />");
+  $("#sb").append("<hr>");
+  $("#sb").append("<div class='btn-group btn-group-justified'></div>");
+  $(".btn-group").append("<a class='btn btn-xs btn-default disabled' id='REPLY_BUTTON'>Reply</a>");
+  $(".btn-group").append("<a class='btn btn-xs btn-default' id='BOARDS_BUTTON'>Boards</a>");
+
+    //Add sidebar submit area.
+    $("#sb").append("<div id='SUBMIT' class='panel-collapse'><br></div>");
+    $("#SUBMIT").append("<div class='col-xs-12 well well-sm' id='SUBMIT_HOLDER'></div>");
+    $("#SUBMIT_HOLDER").append("<input type='text' class='form-control' placeholder='Options' name='email'><br>");
+    $("#SUBMIT_HOLDER").append("<textarea class='form-control' rows='5' name='com'></textarea><br>");
+    $("#SUBMIT_HOLDER").append("<img src='http://bit-player.org/wp-content/uploads/2010/11/recaptcha-example.png' width='100%' /><br>");
+    $("#SUBMIT_HOLDER").append("<input type='text' class='form-control' placeholder='ReCAPTCHA' name='recaptcha_response_field'><br>");
+    $("#SUBMIT_HOLDER").append("<input type='submit' class='btn btn-sm btn-block btn-primary' value='Post'>");
+
+    //Add sidebar boards area.
+
+  //Add functionality to the sidebar.
+  $("#REPLY_BUTTON").click(function() {
+    $("#BOARDS").hide();
+    $("#SUBMIT").show();
+    $("#REPLY_BUTTON").addClass("disabled");
+    $("#BOARDS_BUTTON").removeClass("disabled");
+  });
+  $("#BOARDS_BUTTON").click(function() {
+    $("#SUBMIT").hide();
+    $("#BOARDS").show();
+    $("#BOARDS_BUTTON").addClass("disabled");
+    $("#REPLY_BUTTON").removeClass("disabled");
+  });
+
+  //Put in thread statistics
+  $(".thread").prepend(THREAD_STATS[0] + " Replies / " + THREAD_STATS[1] + " Images / " + THREAD_STATS[2] + " Page<br>");
+
+  //Style Posts
+  $(".postContainer .post").addClass("panel panel-default");
+  var $blockquote = $(".postContainer blockquote");
+  $blockquote.replaceWith(function () {
+      return $('<div/>', {
+          class: 'panel-body',
+          html: this.innerHTML
+      });
+  });
+  $(".postContainer .postInfo").addClass("panel-heading");
+  $(".postContainer .panel-heading").removeClass("postInfo");
+  $(".postContainer .file").addClass("col-sm-3 pull-right");
+  $(".postContainer .file img").attr("style", "max-width:100%;");
+  $(".postContainer .sideArrows").remove();
+}
